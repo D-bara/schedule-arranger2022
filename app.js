1ce178cc67fb184eace8ef079e3d473dda721daf
D-bara <d.bara613677@gmail.com>
2022-10-05 18:48:27 +0900

    render対応

--- app.js
+++ app.js
@@ -3,11 +3,64 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
+const session = require('express-session');
+const passport = require('passport');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//モデルの読み込み
+const User         = require('./models/user');
+const Schedule     = require('./models/schedule');
+const Availability = require('./models/availability');
+const Candidate    = require('./models/candidate');
+const Comment      = require('./models/comment');
+User.sync().then(async () => {
+  Schedule.belongsTo(User, {foreignKey: 'createdBy'});
+  Schedule.sync();
+  Comment.belongsTo(User, {foreignKey: 'userId'});
+  Comment.sync();
+  Availability.belongsTo(User, {foreignKey: 'userId'});
+  await Candidate.sync();
+  Availability.belongsTo(Candidate, {foreignKey: 'candidateId'});
+  Availability.sync();
+});
+
+const GitHubStrategy = require('passport-github2').Strategy;
+const GITHUB_CLIENT_ID = '553f5c30149f89ed571f';
+const GITHUB_CLIENT_SECRET = '07f3bdff8ad49a5fb6fb0f66aba01cf512f992c4';
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback'
+},
+  function (accessToken, refreshToken, profile, done) {
+    process.nextTick(async function () {
+      await User.upsert({
+        userId: profile.id,
+        username: profile.username
+      });
+      done(null, profile);
+    });
+  }
+));
+
+const indexRoute           = require('./routes/index');
+const loginRouter          = require('./routes/login');
+const logoutRouter         = require('./routes/logout');
+const scheduleRouter       = require('./routes/schedules');
+const availabilitiesRouter = require('./routes/availabilities');
+const commentsRouter       = require('./routes/comments');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +72,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({ secret: '0c2a9084a16cd119', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRoute);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+app.use('/schedules', scheduleRouter);
+app.use('/schedules', availabilitiesRouter);
+app.use('/schedules', commentsRouter);
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  function (req, res) {
+});
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    const loginFrom = req.cookies.loginFrom;
+    // オープンリダイレクタ脆弱性対策
+    if(loginFrom && loginFrom.startsWith('/')) {
+      res.clearCookie('loginFrom');
+      res.redirect(loginFrom);
+    } else {
+      res.redirect('/');
+    }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
